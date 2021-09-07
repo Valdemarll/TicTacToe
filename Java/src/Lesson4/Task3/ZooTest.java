763c4f93ca2791f1a460d73e39568d21121101b3
Valdemarll <vladimirsmirnov69706@gmail.com>
2021-09-07 20:50:25 +0300

    Commit

---
+++ Java/src/Lesson4/Task3/ZooTest.java
@@ -0,0 +1,16 @@
+package Lesson4.Task3;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class ZooTest {
+    @Test
+    public void testZoo() {
+        Animal[] animals = {new Monkey(), new Lion(), new Penguin(), new Elephant(), new Zebra()};
+        Zoo zoo = new Zoo(animals);
+
+        assertEquals("Monkey\nLion\nPenguin\nElephant\nZebra\n", zoo.printAnimals());
+        assertEquals("Monkey\nElephant\nZebra\n", zoo.printHerbivoreAnimals());
+        assertEquals("Monkey\nLion\nPenguin\n", zoo.printFleshEatingAnimals());
+    }
+}
