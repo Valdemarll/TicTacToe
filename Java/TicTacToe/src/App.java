de4aa65b4fee304f5f327a3336c55655b8a619e6
Valdemarll <vladimirsmirnov69706@gmail.com>
2021-09-13 23:23:19 +0300

    Commit

---
+++ Java/TicTacToe/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        TicTac ticTac = new TicTac(new String[][]{{"x", "x", " "}, {" ", " ", " "}, {" ", " ", " "}});
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println(ticTac.printOrder());
+            System.out.println(ticTac);
+            if (ticTac.step(ticTac.input())){
+                if (ticTac.checkEnd()) {
+                    System.out.println(ticTac);
+                    System.out.println("Продолжить игру?");
+                    String end = scanner.nextLine();
+                    if (Objects.equals(end, "Нет")) {
+                        break;
+                    } else {
+                        ticTac = new TicTac(new String[][]{{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}});
+                    }
+                }
+            }
+        }
+
+    }
+}
